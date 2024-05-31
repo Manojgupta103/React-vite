be7c3035e8903c0f21437bee2b07d5226823c2d8
Manoj <manoj.gupta16539@sakec.ac.in>
2024-05-31 23:37:58 +0530

    Notification Components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,12 +3,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-import { useState } from 'react';
+// import { useState } from 'react';
 // import Header from './Header';
 import './index.css';
 // import { Col, Container, Row } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import { questions } from './data/faqQuestion';
+// import Button from 'react-bootstrap/Button';
+// import { questions } from './data/faqQuestion';
+import 'react-notifications/lib/notifications.css';
+import { NotificationContainer } from 'react-notifications';
 // import { blog } from './data/blog';
 // import Card from 'react-bootstrap/Card';
 // import { blog } from './data/blog';
@@ -150,73 +152,82 @@ import { questions } from './data/faqQuestion';
 // }
 // export default ternary;
 
-function ShowPassword(){
-  let [pstatus,setPstatus]=useState(false);
+// function ShowPassword(){
+//   let [pstatus,setPstatus]=useState(false);
 
-  return(
-    <div className='App'>
-      <input type={pstatus ? 'text' : "password"}/> 
-      <button onClick={() => setPstatus(!pstatus)}>
-      {pstatus ? "Hide" : "Show"}
-      </button>
-      <br></br>
-    </div>
-  )
-}
+//   return(
+//     <div className='App'>
+//       <input type={pstatus ? 'text' : "password"}/> 
+//       <button onClick={() => setPstatus(!pstatus)}>
+//       {pstatus ? "Hide" : "Show"}
+//       </button>
+//       <br></br>
+//     </div>
+//   )
+// }
+
+// function ResponsiveMenu() {
+//   let [menuStatus, setMenuStatus] = useState(questions[0].id) 
+//   let [showAns, setShowAns] = useState(false);
+
+//   const toggleMenu = () => {
+//     setMenuStatus(!menuStatus);
+//   };
+// let [modalStatus,setModalStatus] = useState(false);
+//   return (
+//     <div className="App">
+//       <div>
+//         <h1>Frequently Asked Questions</h1>
+//         <div className='faqouter'>
+
+// {questions.map((faqitems)=>{
+//   return (
+//     // eslint-disable-next-line react/jsx-key
+//     <div className='faqitems' key={faqitems.id}>
+//       <h2 onClick={()=>setShowAns(faqitems.id)}>{faqitems.question}</h2>
+//       <p className={showAns==faqitems.id ? 'activesAns' : ''}>{faqitems.answer}</p>
+//     </div>
+//   )
+// })}
+//         </div>
+//       </div>
+//       <button className="en" onClick={() => setModalStatus(true)}>Enquire Now</button>
+      
+//       <div onClick={()=>setModalStatus(false)} className={`modalOverLay ${modalStatus?'modalShow':''}`}>
+//       <div className={`ModalDiv ${modalStatus?'showModalDiv':''}`}>
+//           <h3>Enquiry Now <span onClick={() => setModalStatus(false)}>&times;</span></h3>
+//         </div>
+
+//       </div>
+
+// <h1 className="text-3xl font-bold underline text-blue-400">
+//       Hello world!
+//     </h1>
+//       <Button className="micon" onClick={toggleMenu}>
+//         {menuStatus ? <span>&times;</span> : <span>&#9776;</span>}
+//       </Button>
+//       <div className={`menu ${menuStatus ? 'activeMenu' : ''}`}>
+//         <li>Home</li>
+//         <li>About</li>
+//         <li>Course</li>
+//         <li>Gallery</li>
+//         <li>Contact</li>
+//         <li>Carrer</li>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ResponsiveMenu; ShowPassword;
 
 
-
-
-function ResponsiveMenu() {
-  let [menuStatus, setMenuStatus] = useState(questions[0].id) 
-  let [showAns, setShowAns] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuStatus(!menuStatus);
-  };
-let [modalStatus,setModalStatus] = useState(false);
+function Notification() {
   return (
     <div className="App">
-      <div>
-        <h1>Frequently Asked Questions</h1>
-        <div className='faqouter'>
-
-{questions.map((faqitems)=>{
-  return (
-    // eslint-disable-next-line react/jsx-key
-    <div className='faqitems'>
-      <h2 onClick={()=>setShowAns(faqitems.id)}>{faqitems.question}</h2>
-      <p className={showAns==faqitems.id ? 'activesAns' : ''}>{faqitems.answer}</p>
-    </div>
-  )
-})}
-        </div>
-      </div>
-      <button className="en" onClick={() => setModalStatus(true)}>Enquire Now</button>
-      
-      <div onClick={()=>setModalStatus(false)} className={`modalOverLay ${modalStatus?'modalShow':''}`}>
-      <div className={`ModalDiv ${modalStatus?'showModalDiv':''}`}>
-          <h3>Enquiry Now <span onClick={() => setModalStatus(false)}>&times;</span></h3>
-        </div>
-
-      </div>
-
-<h1 className="text-3xl font-bold underline text-blue-400">
-      Hello world!
-    </h1>
-      <Button className="micon" onClick={toggleMenu}>
-        {menuStatus ? <span>&times;</span> : <span>&#9776;</span>}
-      </Button>
-      <div className={`menu ${menuStatus ? 'activeMenu' : ''}`}>
-        <li>Home</li>
-        <li>About</li>
-        <li>Course</li>
-        <li>Gallery</li>
-        <li>Contact</li>
-        <li>Carrer</li>
-      </div>
+      <NotificationContainer />
+      <button>Save</button>
     </div>
   );
 }
+export default Notification;
 
-export default ResponsiveMenu; ShowPassword;
